6311f797c883257c33fc461637d4b31fb866d5eb
Chogo33 <chogovadzetornike21@gmail.com>
2024-05-26 13:16:17 +0400

    goa homeworks

---
+++ level1/homework/davaleba1.py
@@ -0,0 +1,5 @@
+name = "თორნიკე"
+surname = "ჩოგოვაძე" 
+age = 15
+height = 176.5
+print(" მე ვარ " + name + " " + surname + ", ვარ " + str(age) + " წლის ვარ " + str(height) + " სიმაღლის ")
---
+++ level3/homework/moqmedebebi.py
@@ -0,0 +1,8 @@
+four=10 #aq cvladi shemovitane four risi mnishvnelobac aris 10,
+five=20 #aq five-is mnishvneloba ari 20
+print(five - four) #aq 20-10 radgan five=20 da four=10
+print(five/four) #aq gaiyo 20/10 rac aris 2
+print(five * four) #aq gamravlda da miviget 200
+
+           #2 davaleba
+print(five + four) #aq 30 radgan daemata
